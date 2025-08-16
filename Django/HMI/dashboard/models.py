fcb3d7d19fd444f3d79b2de1cb45a74f4350b98c
Bolinfel <bolinfel@gmail.com>
2025-08-16 00:14:09 -0300

    created table to use in dashboard

--- Django/HMI/dashboard/models.py
+++ Django/HMI/dashboard/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Datalog(models.Model):
+    TIMESTAMP = models.DateTimeField(verbose_name="TimeStamp", primary_key=True)
+    SERVO_MOTOR_RAW = models.IntegerField(verbose_name="Servo Motor Reading")
+    SERVO_MOTOR_SCA = models.FloatField(verbose_name="Servo Motor Angle")
+    SERVO_RAW = models.IntegerField(verbose_name="Sensor Reading")
+    SERVO_SCA = models.FloatField(verbose_name="Sensor Distance")
+    ARDUINO_RPI_STATUS = models.BooleanField(verbose_name="Serial Connection Status")
+    ARDUINO_STATUS = models.BooleanField(verbose_name="Status Arduino")
+    RPI_STATUS = models.BooleanField(verbose_name="Status Raspberry")
